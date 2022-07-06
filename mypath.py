20f27d7660e752d5533a0f40d8f2425bbf884219
amir <42087527+AmirMansurian@users.noreply.github.com>
2022-07-06 23:41:21 +0800

    Update mypath.py

--- mypath.py
+++ mypath.py
@@ -5,7 +5,7 @@ class Path(object):
     @staticmethod
     def db_root_dir(dataset):
         if dataset == 'pascal':
-            return '/path/to/datasets/VOCdevkit/VOC2012/'  # folder that contains VOCdevkit/.
+            return '../input/pascal-voc-2012/VOC2012/'  # folder that contains VOCdevkit/.
         elif dataset == 'sbd':
             return '/path/to/datasets/benchmark_RELEASE/'  # folder that contains dataset/.
         elif dataset == 'cityscapes':
