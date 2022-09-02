2f3a4dd405116a197ebb01e3dd753eeb65d9e714
amir <42087527+AmirMansurian@users.noreply.github.com>
2022-09-02 09:49:50 +0100

    Update distiller.py

--- distiller.py
+++ distiller.py
@@ -3,7 +3,23 @@ import torch.nn as nn
 import torch.nn.functional as F
 from scipy.stats import norm
 import scipy
+import numpy as np
 import math
+
+
+from pytorch_grad_cam import GradCAM
+from pytorch_grad_cam.utils.image import show_cam_on_image, preprocess_image
+
+class SemanticSegmentationTarget:
+    def __init__(self, category, mask):
+        self.category = category
+        self.mask = torch.from_numpy(mask)
+        if torch.cuda.is_available():
+            self.mask = self.mask.cuda()
+        
+    def __call__(self, model_output):
+        return (model_output[self.category:, :, : ] * self.mask).sum()
+
 
 
 
@@ -61,6 +77,19 @@ def get_margin_from_BN(bn):
 
     return torch.FloatTensor(margin).to(std.device)
 
+
+
+class SemanticSegmentationTarget:
+    def __init__(self, category, mask):
+        self.category = category
+        self.mask = torch.from_numpy(mask)
+        if torch.cuda.is_available():
+            self.mask = self.mask.cuda()
+        
+    def __call__(self, model_output):
+        return (model_output[self.category, :, : ] * self.mask).sum()
+
+
 class Distiller(nn.Module):
     def __init__(self, t_net, s_net, args):
         super(Distiller, self).__init__()
@@ -78,18 +107,38 @@ class Distiller(nn.Module):
         self.t_net = t_net
         self.s_net = s_net
         self.args = args
+        self.t_net.eval()
 
+        #print(t_net)
+
+        self.loss_divider = [8, 4, 2, 1, 1, 4*4]
         self.criterion = sim_dis_compute
         self.temperature = 1
         self.scale = 0.5
 
+
+    def gcam(self, image, out, net, sem_class_to_idx, cls): 
+      car_category = sem_class_to_idx[cls]
+      car_mask = out[0, :, :, :].argmax(axis=0).detach().cpu().numpy()
+      car_mask_float = np.float32(car_mask == car_category)
+
+      target_layers = [net.aspp.conv1]
+      targets = [SemanticSegmentationTarget(car_category, car_mask_float)]
+      with GradCAM(model=net,
+                        target_layers=target_layers,
+                        use_cuda=torch.cuda.is_available()) as cam:
+                grayscale_cam = cam(input_tensor=torch.unsqueeze(image, 0),
+                                    targets=targets)[0, :]
+
+      return grayscale_cam
+
+
     def forward(self, x):
 
-        t_feats, t_out, dist_t = self.t_net.extract_feature(x)
-        s_feats, s_out, dist_s = self.s_net.extract_feature(x)
+        t_feats, t_out = self.t_net.extract_feature(x)
+        s_feats, s_out = self.s_net.extract_feature(x)
         feat_num = len(t_feats)
-        
-        
+
         pa_loss = 0 
         if self.args.pa_lambda is not None: # pairwise loss
           feat_T = t_feats[4]
@@ -110,6 +159,23 @@ class Distiller(nn.Module):
         lo_loss = 0
         if self.args.lo_lambda is not None: #logits loss
           lo_loss =  self.args.lo_lambda * torch.nn.KLDivLoss()(F.log_softmax(s_out / self.temperature, dim=1), F.softmax(t_out / self.temperature, dim=1))
-        
 
-        return s_out, pa_loss, pi_loss, lo_loss
+
+        sem_classes = [
+              '__background__', 'aeroplane', 'bicycle', 'bird', 'boat', 'bottle', 'bus',
+              'car', 'cat', 'chair', 'cow', 'diningtable', 'dog', 'horse', 'motorbike',
+              'person', 'pottedplant', 'sheep', 'sofa', 'train', 'tvmonitor'
+          ]
+        sem_class_to_idx = {cls: idx for (idx, cls) in enumerate(sem_classes)}
+        
+        gcam_loss = 0
+        if self.args.gcam_lambda is not None:
+          for i in range(4) : 
+            for cls in sem_classes : 
+              t_cam = self.gcam(x[i], t_out, self.t_net, sem_class_to_idx, cls)
+              s_cam = self.gcam(x[i], s_out, self.s_net, sem_class_to_idx, cls)
+              gcam_loss += (F.normalize(torch.from_numpy(t_cam)) - F.normalize(torch.from_numpy(s_cam))).pow(2).mean()
+          gcam_loss *= self.args.gcam_lambda  
+         
+            
+        return s_out, pa_loss, pi_loss, lo_loss, gcam_loss
