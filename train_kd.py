eb54c0bf3e1dd3a59608ca7ce2e9397a92eabf47
amir <42087527+AmirMansurian@users.noreply.github.com>
2022-10-27 08:46:53 +0100

    Update train_kd.py

--- train_kd.py
+++ train_kd.py
@@ -122,9 +122,10 @@ class Trainer(object):
             self.scheduler(optimizer, i, epoch, self.best_pred)
             optimizer.zero_grad()
             
-            output, pi_loss = self.d_net(image)
+            output, loss_distill = self.d_net(image)
             loss_seg = self.criterion(output, target)
-            loss = loss_seg + 100 * pi_loss
+            #loss = loss_seg + 100 * pi_loss
+            loss = loss_seg + loss_distill.sum() / batch_size * 1e-5
             
             loss.backward()
             optimizer.step()
