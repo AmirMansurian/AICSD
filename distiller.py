7bb3cfa655b4963da6d252046078127363a685ba
amir <42087527+AmirMansurian@users.noreply.github.com>
2023-03-21 18:34:08 +0100

    Update distiller.py

--- distiller.py
+++ distiller.py
@@ -109,6 +109,23 @@ class Distiller(nn.Module):
         
         lo_loss = 0
         if self.args.lo_lambda is not None: #logits loss
-          lo_loss =  self.args.lo_lambda * torch.nn.KLDivLoss()(F.log_softmax(s_out / self.temperature, dim=1), F.softmax(t_out / self.temperature, dim=1))
+          #lo_loss =  self.args.lo_lambda * torch.nn.KLDivLoss()(F.log_softmax(s_out / self.temperature, dim=1), F.softmax(t_out / self.temperature, dim=1))
+          b, c, h, w = s_out.shape
+          s_logit = torch.reshape(s_out, (b, c, h*w))
+          t_logit = torch.reshape(t_out, (b, c, h*w))
+
+          s_logit = F.softmax(s_logit / self.temperature, dim=2)
+          t_logit = F.softmax(t_logit / self.temperature, dim=2)
+          kl = torch.nn.KLDivLoss(reduction="batchmean")
+
+          ICCS = torch.empty((21,21))
+          ICCT = torch.empty((21,21))
+
+          for i in range(21):
+            for j in range(21):
+              ICCS[i, j] = kl(s_logit[:, i], s_logit[:, j])
+              ICCT[i, j] = kl(t_logit[:, i], t_logit[:, j])
+
+          lo_loss =  self.args.lo_lambda * (ICCS - ICCT).pow(2).mean() 
 
         return s_out, pa_loss, pi_loss, lo_loss
