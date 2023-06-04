5982801a0ada72594b450fc79b17e623e37eea43
amir <42087527+AmirMansurian@users.noreply.github.com>
2023-06-04 13:48:01 +0100

    Update for Cityscapes

--- mypath.py
+++ mypath.py
@@ -9,7 +9,7 @@ class Path(object):
         elif dataset == 'sbd':
             return '/path/to/datasets/benchmark_RELEASE/'  # folder that contains dataset/.
         elif dataset == 'cityscapes':
-            return '/path/to/datasets/cityscapes/'     # foler that contains leftImg8bit/
+            return '/kaggle/input/cityscapes/Cityspaces'     # foler that contains leftImg8bit/
         elif dataset == 'coco':
             return '/path/to/datasets/coco/'
         else:
