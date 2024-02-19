3bbcb31bef8c249114c877a4451d0673a0484d55
amir <42087527+AmirMansurian@users.noreply.github.com>
2024-02-19 17:30:43 +0330

    Update distiller.py

--- distiller.py
+++ distiller.py
@@ -6,33 +6,6 @@ import scipy
 import numpy as np
 import math
 
-
-def L2(f_):
-    return (((f_**2).sum(dim=1))**0.5).reshape(f_.shape[0],1,f_.shape[2],f_.shape[3]) + 1e-8
-
-def similarity(feat):
-    feat = feat.float()
-    tmp = L2(feat).detach()
-    feat = feat/tmp
-    feat = feat.reshape(feat.shape[0],feat.shape[1],-1)
-    return torch.einsum('icm,icn->imn', [feat, feat])
-
-def sim_dis_compute(f_S, f_T):
-    sim_err = ((similarity(f_T) - similarity(f_S))**2)/((f_T.shape[-1]*f_T.shape[-2])**2)/f_T.shape[0]
-    sim_dis = sim_err.sum()
-    return sim_dis
-
-def distillation_loss(source, target, margin):
-    target = torch.max(target, margin)
-    loss = torch.nn.functional.mse_loss(source, target, reduction="none")
-    loss = loss * ((source > target) | (target > 0)).float()
-    return loss.sum()
-
-def dist_loss(source, target):
-    loss = torch.nn.functional.mse_loss(source, target, reduction="none")
-    loss = loss * ((source > target) | (target > 0)).float()
-    return loss.sum()
-
 def build_feature_connector(t_channel, s_channel):
     C = [nn.Conv2d(s_channel, t_channel, kernel_size=1, stride=1, padding=0, bias=False),
          nn.BatchNorm2d(t_channel)]
@@ -93,6 +66,7 @@ class Distiller(nn.Module):
         lad_loss = 0
         for i in range(3, feat_num):
             b,c,h,w = t_feats[i].shape
+            s_feats[i] = self.Connectors[i](s_feats[i])
             lad_loss += (s_feats[i] / torch.norm(s_feats[i], p = 2) - t_feats[i] / torch.norm(t_feats[i], p = 2)).pow(2).sum() / (b) * 10
         
 
