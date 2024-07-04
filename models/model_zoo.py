731b237aed5e9bcc5b6e93d9f6133ffa560ac7bb
amir <42087527+AmirMansurian@users.noreply.github.com>
2024-07-04 16:19:46 +0330

    Update model_zoo.py

--- models/model_zoo.py
+++ models/model_zoo.py
@@ -5,6 +5,7 @@ from .deeplabv3 import *
 from .deeplabv3_mobile import *
 from .psp_mobile import *
 
+
 __all__ = ['get_segmentation_model']
 
 
@@ -14,5 +15,6 @@ def get_segmentation_model(model, **kwargs):
         'deeplabv3': get_deeplabv3,
         'deeplab_mobile': get_deeplabv3_mobile,
         'psp_mobile': get_psp_mobile,
+        'segformer': get_segformer,
     }
     return models[model](**kwargs)
